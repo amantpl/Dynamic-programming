c723020219922cefd1204e6bfe7d1cd6371354a6
Aman Thapliyal <kira@Amans-MacBook-Air.local>
2024-09-13 18:36:31 +0530

    Added longest common subsequence using recursive appraoch having time limit exceeded

---
+++ lcs1.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+const int mod = 1e9 + 7;
+using namespace std;
+
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution {
+  public:
+    
+// Function to find the length of longest common subsequence in two strings.
+int solve(int n,int m,string str1,string str2){
+    if(n==0 || m==0){
+        return 0;
+    }
+    if(str1[n-1]==str2[m-1]){
+        return (1+solve(n-1,m-1,str1,str2));
+    }else{
+        return max(solve(n-1,m,str1,str2),solve(n,m-1,str1,str2));
+    }
+}
+    int lcs(int n, int m, string str1, string str2) {
+        return solve(n,m,str1,str2);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t, n, m;
+    cin >> t;
+    while (t--) {
+        cin >> n >> m; // Take size of both the strings as input
+        string str1, str2;
+        cin >> str1 >> str2; // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(n, m, str1, str2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
